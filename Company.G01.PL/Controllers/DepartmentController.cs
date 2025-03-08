61cea3a92408fe670dbe39644ddb7092c0bdc1eb
l340 <l340@DESKTOP-H47FA4Q>
2025-03-09 00:56:50 +0200

    Create Create(Action-view)

--- Company.G01.PL/Controllers/DepartmentController.cs
+++ Company.G01.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using Company.G02.BLL.Interfices;
+﻿using Company.G01.DAL.Models;
+using Company.G01.PL.Dtos;
+using Company.G02.BLL.Interfices;
 using Company.G02.BLL.Repositres;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +21,33 @@ namespace Company.G01.PL.Controllers
            
             var departments = _departmentRepositry.GetAll();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code= model.Code,
+                    Name= model.Name,
+                    CreateAt=model.CreateAt
+
+                };
+              var Count =  _departmentRepositry.Add(department);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            return View(model);
         }
     }
 }
---
+++ Company.G01.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Company.G01.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime CreateAt { get; set; }
+    }
+}
