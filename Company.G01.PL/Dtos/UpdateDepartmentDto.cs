2fe950a153ef2365002566aa05d57efc50b129df
l340 <l340@DESKTOP-H47FA4Q>
2025-03-10 20:17:08 +0200

    Add Implementation Of Details , Edit ,And Delete For Department Serivece

---
+++ Company.G01.PL/Dtos/UpdateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G01.PL.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime CreateAt { get; set; }
+    }
+}
