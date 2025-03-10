e7ae7f3cfc3cf51fd8286581d73add03f36eb7f6
l340 <l340@DESKTOP-H47FA4Q>
2025-03-10 21:10:23 +0200

    Implementation Of BLL For Employee Serivece Before Generic

---
+++ Company.G02.BLL/Interfices/IEmployeeRepositry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G01.DAL.Models;
+
+namespace Company.G02.BLL.Interfices
+{
+    public interface IEmployeeRepositry
+    {
+
+        IEnumerable<Employee> GetAll();
+        Employee? Get(int id);
+
+        int Add(Employee model);
+        int Update(Employee model);
+        int Delete( Employee model);
+    }
+}
---
+++ Company.G02.BLL/Repositres/EmployeeRepositry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G01.DAL.Data.Contexts;
+using Company.G01.DAL.Models;
+using Company.G02.BLL.Interfices;
+
+namespace Company.G02.BLL.Repositres
+{
+    public class EmployeeRepositry : IEmployeeRepositry
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepositry(CompanyDbContext context)
+        {
+              _context = context;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee? Get(int id)
+        {
+          return _context.Employees.Find(id);
+        }
+
+        public int Add(Employee model)
+        {
+             _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee model)
+        {
+
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+        public int Delete(Employee model)
+        {
+
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+       
+
+        
+    }
+}
